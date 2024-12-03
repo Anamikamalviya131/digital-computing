a610d1e4661d2bb60d261479fbdeb27bcdee5885
Anamikamalviya131 <anamikamalviya20@gmail.com>
2024-12-03 18:56:53 +0530

    Add files via upload

---
+++ calc.js
@@ -0,0 +1,84 @@
+var x,y,result
+function input1()
+{
+    document.getElementById("inputbox").value += "1"
+}
+function input2()
+{
+    document.getElementById("inputbox").value += "2"
+}
+function input3()
+{
+    document.getElementById("inputbox").value += "3"
+}
+function input4()
+{
+    document.getElementById("inputbox").value += "4"
+}
+function input5()
+{
+    document.getElementById("inputbox").value += "5"
+}
+function input6()
+{
+    document.getElementById("inputbox").value += "6"
+}
+function input7()
+{
+    document.getElementById("inputbox").value += "7"
+}
+function input8()
+{
+    document.getElementById("inputbox").value += "8"
+}
+function input9()
+{
+    document.getElementById("inputbox").value += "9"
+}
+function add()
+{
+    document.getElementById("inputbox").value += "+"
+}
+function sub()
+{
+    document.getElementById("inputbox").value += "-"
+}
+function mult()
+{
+    document.getElementById("inputbox").value += "*"
+}
+function div()
+{
+    document.getElementById("inputbox").value += "/"
+}
+function equal()
+{
+    document.getElementById("inputbox").value = eval(document.getElementById("inputbox").value)
+} 
+function sin()
+{
+    document.getElementById("inputbox").value = Math.sin(document.getElementById("inputbox").value)
+}
+function cos()
+{
+    document.getElementById("inputbox").value= Math.cos(document.getElementById("inputbox").value)
+}
+function tan()
+{
+    document.getElementById("inputbox").value = Math.tan(document.getElementById("inputbox").value)
+}
+function sec()
+{
+    document.getElementById("inputbox").value =Math.sec(document.getElementById("inputbox").value)
+}
+function cosec()
+{
+    document.getElementById("inputbox").value =Math.cosec(document.getElementById("inputbox").value)
+}
+function cot()
+{
+    document.getElementById("inputbox").value =Math.cot(document.getElementById("inputbox").value)
+}
+function dot(){
+    document.getElementById("inputbox").value += "."
+}
